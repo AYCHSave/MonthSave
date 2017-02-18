a766a71a5361d006697d0a5ad6a46545f50e952e
Gustavo Sobral <ghsobral@gmail.com>
2017-02-18 15:55:44 -0200

    Create User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218174013) do
+ActiveRecord::Schema.define(version: 20170218175423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20170218174013) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["owner_id_id"], name: "index_projects_on_owner_id_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "email"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
