5bb2e1bef85801adcc8d331d0b44af1c6b7b0603
Fernando Brito <email@fernandobrito.com>
2017-02-18 17:50:42 -0300

    Add dashboard widget to show user contributions

--- app/controllers/my/dashboard_controller.rb
+++ app/controllers/my/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class My::DashboardController < ApplicationController
   def index
-    
+    @contributing_projects = current_user.contributing_projects
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :own_projects, class_name: 'Project', foreign_key: 'owner_id'
-  has_many :contribuiting_projects, class_name: 'ProjectUser', foreign_key: 'user_id'
+  has_many :contributing_projects, class_name: 'ProjectUser', foreign_key: 'user_id'
 end
