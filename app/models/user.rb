78ab0fbbfa4b2fb6f7fa8168dadb8df63f34f3fa
Fernando Brito <email@fernandobrito.com>
2017-02-18 16:47:25 -0300

    Add initial index view for projects

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :own_projects, class_name: 'Project', foreign_key: 'owner_id'
+  has_many :contribuiting_projects, class_name: 'ProjectUser', foreign_key: 'user_id'
 end
