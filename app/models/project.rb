f44f75f07729ee12c6295a634eafbd9e70560dd0
Fernando Brito <email@fernandobrito.com>
2017-02-18 18:11:15 -0300

    Add contributing users to project show

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :owner, class_name: 'User'
+
+  has_many :project_users
+  has_many :contributing_users, class_name: 'User', through: :project_users, source: :user
 end
