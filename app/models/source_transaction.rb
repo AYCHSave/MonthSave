3523e9b8bd89925de3ab019b0d84352ba61baf12
Gustavo Sobral <ghsobral@gmail.com>
2017-02-18 23:22:42 -0300

    Add transaction_date to SourceTransaction

--- app/models/source_transaction.rb
+++ app/models/source_transaction.rb
@@ -2,13 +2,14 @@
 #
 # Table name: source_transactions
 #
-#  id          :integer          not null, primary key
-#  external_id :string
-#  description :string
-#  price_cents :integer
-#  account_id  :integer
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id               :integer          not null, primary key
+#  external_id      :string
+#  description      :string
+#  price_cents      :integer
+#  account_id       :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  transaction_date :datetime
 #
 # Indexes
 #
@@ -19,4 +20,6 @@ class SourceTransaction < ApplicationRecord
   monetize :price_cents, as: 'price'
 
   belongs_to :account, class_name: 'SourceAccount'
+
+  validates :external_id, uniqueness: true
 end
