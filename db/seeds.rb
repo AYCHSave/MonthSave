4f7b55ad8ef1097a576c8c80a986c8f38559a45d
Fernando Brito <email@fernandobrito.com>
2017-02-18 21:01:40 -0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+WithdrawalAccount.delete_all
 ProjectUser.delete_all
 Project.delete_all
 Saving.delete_all
