6bd335010ff172e499cbc2e4690654fb215d0cdc
David Guilherme <davidlguilherme@gmail.com>
2017-02-19 01:41:39 -0300

    Create a helper to create navigation links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def coin_bank_savings_total(coin_bank)
     "R$ #{coin_bank.savings.map(&:price).sum}"
   end
+
+  def nav_link(link_path, &block)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_path, &block
+    end
+  end
 end
