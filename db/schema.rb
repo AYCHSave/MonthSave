fd84a040b7eb6c969c74e1d55eb803feec3fc92e
Fernando Brito <email@fernandobrito.com>
2017-02-18 23:43:52 -0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170219010112) do
-
+ActiveRecord::Schema.define(version: 20170219014509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +35,7 @@ ActiveRecord::Schema.define(version: 20170219010112) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
-    t.string   "description"
+    t.text     "description"
     t.string   "image_url"
     t.integer  "owner_id"
     t.datetime "created_at",  null: false
