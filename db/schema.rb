058a9ef76285625c6117663beab75a5fa52a4bc2
Fernando Brito <email@fernandobrito.com>
2017-02-19 02:02:27 -0300

    Add public field to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170219014509) do
+ActiveRecord::Schema.define(version: 20170219045811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,9 @@ ActiveRecord::Schema.define(version: 20170219014509) do
     t.text     "description"
     t.string   "image_url"
     t.integer  "owner_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "public",      default: false
     t.index ["owner_id"], name: "index_projects_on_owner_id", using: :btree
   end
 
